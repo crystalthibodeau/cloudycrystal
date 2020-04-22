393d10a8dc1e20e6023037b27380e07b43145882
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-04-22 15:56:01 -0500

    landing page response body

---
+++ src/main/java/com/codeup/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.codeup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String landing(){
+        return "Landing";
+    }
+}
