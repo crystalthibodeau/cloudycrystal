f5b73b427752d1a95ccd6f923c6134fd05f60fc4
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-05-06 13:39:38 -0500

    adding models

---
+++ src/main/java/com/codeup/models/Post.java
@@ -0,0 +1,115 @@
+package com.codeup.models;
+
+import javax.persistence.*;
+import java.net.UnknownServiceException;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 50, nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    String postedAt;
+
+    public Post(){
+    }
+
+    //Many to one annotation to User model
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+//    @ManyToMany
+//    @JoinTable(
+//
+//            name = "recipes_categories",
+//            joinColumns = {@JoinColumn(name = "recipe_id")},
+//            inverseJoinColumns = {@JoinColumn(name = "category_id")}
+//    )
+//    private List<Categories> categories;
+
+
+
+    //Many to many annotation to User model for favorites
+//    @ManyToMany(mappedBy = "favorites")
+//    private List<User> favoritedBy;
+
+    //One to many annotation to RecipeImages model
+    @Column(nullable = false)
+    private String postImageUrl;
+
+    //One to many annotation to Comments model
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+//    private List<Comments> comments;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+//    public List<User> getFavoritedBy() {
+//        return favoritedBy;
+//    }
+
+//    public void setFavoritedBy(List<User> favoritedBy) {
+//        this.favoritedBy = favoritedBy;
+//    }
+
+    public User getUser() { return user; }
+
+    public void setUser(User user) { this.user = user;
+    }
+
+    public String getPostImageUrl() {
+        return postImageUrl;
+    }
+
+    public void setPostImageUrl(String postImageUrl) {
+        this.postImageUrl = postImageUrl;
+    }
+
+//    public List<Categories> getCategories() {
+//        return categories;
+//    }
+
+//    public void setCategories(List<Categories> categories) {
+//        this.categories = categories;
+//    }
+
+//    public List<Comments> getComments() {
+//        return comments;
+//    }
+
+//    public void setComments(List<Comments> comments) {
+//        this.comments = comments;
+//    }
+}
+
+
+
+
+
+
---
+++ src/main/java/com/codeup/models/User.java
@@ -0,0 +1,142 @@
+package com.codeup.models;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private long id;
+
+    @Column(length =25, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 25, nullable = false)
+    private String first_name;
+
+    @Column(length = 25, nullable = false)
+    private String last_name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean admin;
+
+    @Column(nullable = false)
+    private String profileImageUrl;
+
+    public User() {
+    }
+
+    //Many to many  relationship connection to recipes table
+//    @ManyToMany(cascade = CascadeType.ALL)
+//    @JoinTable(
+//            name="users_favorites",
+//            joinColumns = {@JoinColumn(name="user_id")},
+//            inverseJoinColumns = {@JoinColumn(name="recipe_id")}
+//    )
+//    private List<Recipe> favorites;
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Post> recipes;
+
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+//    private List<Comments> comments;
+
+    //Copy Constructor
+    public User(User copy){
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+        first_name = copy.first_name;
+        last_name = copy.last_name;
+        profileImageUrl = copy.profileImageUrl;
+        admin = copy.admin;
+    }
+
+    public User(String username, String email, String first_name, String last_name, String password) {
+        this.username = username;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.password = password;
+        this.admin = false;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+}
+
